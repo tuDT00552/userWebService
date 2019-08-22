3f0db0208588efce8bb2023a0d3c36cfad38ef43
ChosHuong <vife@ionemail.net>
2019-08-22 19:07:49 +0700

    ook

---
+++ build/generated-sources/ap-source-output/entity/Users_.java
@@ -0,0 +1,16 @@
+package entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-08-22T16:52:46")
+@StaticMetamodel(Users.class)
+public class Users_ { 
+
+    public static volatile SingularAttribute<Users, String> password;
+    public static volatile SingularAttribute<Users, Integer> role;
+    public static volatile SingularAttribute<Users, Integer> userid;
+    public static volatile SingularAttribute<Users, String> username;
+
+}
